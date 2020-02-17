a087ee66233b379a902656ac1d54f77a6289491e
cosshop\$cosshop <kirildemchenko07@gmail.com>
2020-02-17 21:59:29 +0200

    Create new controller it is a parent of all blog controllers

---
+++ app/Http/Controllers/Blog/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
+
+abstract class BaseController extends Controller
+{
+    //
+
+}

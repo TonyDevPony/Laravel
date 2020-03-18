36bb542940bbce9e0acffeebd7199e73958a5095
cosshop\$cosshop <kirildemchenko07@gmail.com>
2020-03-18 22:57:12 +0200

    add event listener for view success or errors change data

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -6,6 +6,33 @@
         @method('PATCH')
         @csrf
         <div class="container">
+            @php
+                /** @var \Illuminate\Support\ViewErrorBag $errors*/
+            @endphp
+            @if($errors->any())
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <div class="alert alert-danger" role="alert">
+                            <button type="button" data-dismiss="alert" aria-label="Close" class="close">
+                                <span aria-hidden="true">&#215;</span>
+                            </button>
+                            {{ $errors->first() }}
+                        </div>
+                    </div>
+                </div>
+            @endif
+            @if(session('success'))
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <div class="alert alert-success" role="alert">
+                            <button type="button" data-dismiss="alert" aria-label="Close" class="close">
+                                <span aria-hidden="true">&#215;</span>
+                            </button>
+                            {{ session()->get('success') }}
+                        </div>
+                    </div>
+                </div>
+            @endif
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     @include('blog.admin.categories.includes.item_edit_main_col')
